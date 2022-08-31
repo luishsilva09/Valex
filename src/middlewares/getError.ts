c0c750b04d298d440804b239b896be3bb0bae597
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-31 00:43:17 -0300

    [FIX]envio de erro na validacao

--- src/middlewares/getError.ts
+++ src/middlewares/getError.ts
@@ -5,6 +5,8 @@ export default async function getError(error:any,req:Request,res:Response,next:N
     if(error.code === 'NotFound'){
         return res.status(404).send(error.message)
     }
+    if(error.code === 'invalid') return res.status(422).send(error.message)
+    
     console.log(error)
     return res.status(500).send(error)
 }
